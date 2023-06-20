109c2b9a81cbbfee776129e3875df67589be41b4
Anekant <Anekantjainsagar@gmail.com>
2023-06-21 01:24:53 +0530

    Added one image to src

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -13,7 +13,7 @@ const Footer = () => {
     <div className={styles.footer}>
       <div className={styles.content}>
         <div className={styles.logo} onClick={() => history("/")}>
-          <img src="/android-icon-144x144.png" alt="" />
+          <img src="./images/android-icon-144x144.png" alt="" />
           <h1>Flux</h1>
         </div>
         <p>
